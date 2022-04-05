6316cf16f38df0b1a1fdf335147bb0f594f70f2d
nirmit <nirmitjatana38@gmail.com>
2022-04-05 13:44:15 +0530

    feat: custom hook to fetch each character

---
+++ src/hooks/useCharacterSearch.js
@@ -0,0 +1,42 @@
+import { useEffect , useState } from 'react'
+import axios from 'axios'
+
+export default function useCharacterSearch( id ) {
+
+    // setting states for: characters, loading, next, error
+    const [loading , SetLoading] = useState(true)
+    const [error , SetError] = useState(false)
+    const [character , SetCharacter] = useState([])
+
+    // fetching data
+    useEffect(() => {
+        // console.log("nj")
+        SetError(false);
+        axios({
+            method: 'GET',
+            url: 'https://rickandmortyapi.com/api/character/' + id,
+            // cancelToken : new axios.CancelToken(c => cancel = c)
+        })
+        .then(res => {
+            SetCharacter(() => {
+                // console.log(ress.data)
+                return res.data
+            })
+            // check next page
+            SetLoading(false)
+        })
+        .catch(err => {
+            // ignoring cancel error
+            // if(axios.isCancel(err)) return
+            // if(err.response.status === 404){
+            //     // loading remove
+            //     //need to output proper message
+            //     SetLoading(false)
+            //     return
+            // }
+            SetError(true)
+        })
+        // cancel request on unmount
+    },[id])
+    return {loading , error , character}
+}
