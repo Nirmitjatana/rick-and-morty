da9c24c7c9951fc72f4fab5b9a31b69d766e65ca
nirmit <nirmitjatana38@gmail.com>
2022-04-05 17:06:05 +0530

    chore: character page mobile responsive

--- src/Pages/character.js
+++ src/Pages/character.js
@@ -15,21 +15,22 @@ const Character = () => {
 
     return (
         <>
-        <div className="text-white bangers text-4xl mx-20 my-16 cursor-pointer" onClick={() => handleBack()}> &lt; back</div>
-        <div className="fixed -bottom-10 -z-10 w-full">
+        <div className="text-white bangers text-2xl lg:text-4xl mx-10 lg:mx-20 my-16 cursor-pointer" onClick={() => handleBack()}> &lt; back</div>
+        <div className="fixed bottom-0 lg:-bottom-10 -z-10 w-full">
             <img src={banner} alt="banner" className="mx-auto w-[40%]"/>
         </div>
             {
-                loading ? <h1 className="text-white text-center text-4xl my-10">Lemme load! wubba lubba dub dub.</h1> : 
-                <div className="bg-[#49494980] flex flex-cols-2 z-20 h-[400px] w-fit mx-auto">
-                    <div className="w-[400px]">
-                        <img src={character.image} alt="icon" className="w-[400px]"/>
+                loading ? <h1 className="text-white text-center text-2xl lg:text-4xl my-10">Lemme load! wubba lubba dub dub.</h1> : 
+                <div className="bg-[#49494980] lg:flex lg:flex-cols-2 z-20 lg:h-[400px] lg:w-fit w-[300px] h-[600px] mx-auto">
+                    <div className="lg:w-[400px]">
+                        <img src={character.image} alt="icon" className="lg:w-[400px]"/>
                     </div>
-                    <div className="text-white w-[600px]"> 
+
+                    <div className="text-white lg:w-[600px]"> 
                         <div className="text-center">
-                            <p className="bangers text-6xl my-8">{character.name}</p>
+                            <p className="bangers text-3xl lg:text-6xl my-3 lg:my-8">{character.name}</p>
                         </div>
-                        <div className="text-2xl grid grid-cols-2 text-center gap-y-8 my-12">
+                        <div className="lg:text-2xl grid grid-cols-2 text-center gap-y-8 lg:my-12 mr-3">
                             <p><span className="pangolin">Gender:</span> {character.gender}</p>
                             <p><span className="pangolin">Species:</span> {character.species}</p>
                             {
@@ -42,9 +43,9 @@ const Character = () => {
                                 <p><span className="pangolin">Status:</span> <span className="text-[#FFE70D]"> {character.status}</span></p>
                             
                             }
-                            <p className="text-xl"><span className="pangolin">Number of Episodes:</span> {character.episode.length}</p>
-                            <p className="text-xl"><span className="pangolin">Origin:</span> {character.origin.name}</p>
-                            <p className="text-xl"><span className="pangolin">Location:</span> {character.location.name}</p>
+                            <p className="lg:text-xl"><span className="pangolin">Number of Episodes:</span> {character.episode.length}</p>
+                            <p className="lg:text-xl"><span className="pangolin">Origin:</span> {character.origin.name}</p>
+                            <p className="lg:text-xl"><span className="pangolin">Location:</span> {character.location.name}</p>
                         </div>
                     </div>
                 </div> }
