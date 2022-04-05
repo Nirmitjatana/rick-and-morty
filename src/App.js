4a3fd519a8059d302f76b7e9f001f5af0929390f
nirmit <nirmitjatana38@gmail.com>
2022-04-05 12:22:48 +0530

    chore: context setup
    context setup in case needed, for now fetching again

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import Landing from './Pages/landing';
 import Character from './Pages/character';
-
+import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
+// import { UserContext } from './utils/UserContext';
+// import { useState } from 'react';
 const App = () => {
+  // const [ value, SetValue ] = useState(null);
   return (
-    <>
-      {/* <Landing/> */}
-      <Character/>
-    </>
+    <Router>
+      {/* <UserContext.Provider value={{value,SetValue}}> */}
+        <Routes>
+          <Route path="/" element={<Landing/>}/>
+          <Route path="/character/:id" element={<Character/>}/>
+        </Routes>
+      {/* </UserContext.Provider> */}
+    </Router>
   );
 }
 
