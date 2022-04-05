0860211a727d3c523982dd847cb9d2224b5aa5a7
nirmit <nirmitjatana38@gmail.com>
2022-04-05 17:24:34 +0530

    chore: minor

--- src/Pages/landing.js
+++ src/Pages/landing.js
@@ -51,7 +51,7 @@ const Landing = () => {
                 <input type="text" value={query} placeholder="Morty you need to search for it" onChange={handleSearch} className='outline-none px-5 py-2 bg-transparent border-t-0 border-l-0 border-r-0 border-2 border-solid border-[#79BB46] text-white w-[80%] lg:w-[50vw] mb-10 mt-3'/>
                 <img src={dab} alt="dab" className="fixed bottom-0 right-4 w-44"/>
             </div>
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-y-10 gap-x-5 mx-5 lg:mx-24">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-y-10 gap-x-5 lg:mx-24">
                 {characters.map((character, index) => {
                     if(characters.length === index+1)
                         return (
